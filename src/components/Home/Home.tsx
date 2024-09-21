191f3fb911b25ffc9c4e77c8b35e37e29a9fbda5
KskPLab <basha@patternlab.ai>
2024-09-21 19:50:20 +0530

    added avatar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,9 +2,18 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./Home.css";
-import { Avatar, Box, Card, Paper } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  Grid,
+  Grid2,
+  Paper,
+  Typography,
+} from "@mui/material";
 import Tilt from "react-parallax-tilt";
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter } from "react-simple-typewriter";
+import profilePic from "../../assets/profile.jpg";
 
 interface HomeProps {
   // Additional props...
@@ -23,61 +32,105 @@ const Home: React.FC<HomeProps> = ({ home }) => {
     });
   }, []);
 
-  console.log(home);
-
   return (
     <Box
       id="home"
       sx={{
         height: "100vh",
-        p: 10,
-        fontSize: 30,
+        padding: {
+          xs: 2, // Low padding for extra-small screens
+          sm: 10, // Medium padding for small screens and above
+        },
+        fontSize: {
+          xs: 10, // Smaller font size for extra-small screens
+          sm: 30, // Default font size for small screens and above
+        },
       }}
     >
-      <Tilt
-        tiltAngleXInitial={-15}
-        tiltAngleYInitial={15}
-        // tiltMaxAngleX={30}
-        // tiltMaxAngleY={30}
-      >
+      <Tilt tiltAngleXInitial={-15} tiltAngleYInitial={15}>
         <Paper
           sx={{
             p: 5,
-            height: 500,
-            cursor: "pointer",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
             backgroundColor: "#fefae0",
           }}
         >
-          <Avatar sx={{ width: 200, height: 200 }} />
-          <Box sx={{ ml: 10 }}>
-            <h1
-              style={{
-                // paddingTop: "5rem",
-                // margin: "auto 0",
-                // fontWeight: "normal",
-                fontSize: 'large',
-              }}
-            >
-              Welcome, I'm Basha ksk
-              <span style={{ color: "#d4a373", fontWeight: "bold" }}>
-                {/* Style will be inherited from the parent element */}
-                <Typewriter
-                  words={["Eat", "Sleep", "Code", "Repeat!"]}
-                  loop={true}
-                  cursor
-                  cursorStyle="_"
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1000}
-                //   onLoopDone={handleDone}
-                //   onType={handleType}
+          <Grid
+            container
+            spacing={3}
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "flex-start" }, // Center on xs, left align on md
+              alignItems: { xs: "center", md: "start" },
+              height: "100%",
+              textAlign: { xs: "center", md: "left" }, // Center text on xs
+            }}
+          >
+            <Grid item xs={12} md={4}>
+              <Avatar
+                sx={{
+                  width: { xs: 200, sm: 250, md: 350 },
+                  height: { xs: 200, sm: 250, md: 350 },
+                  margin: { xs: "0 auto", md: "0" }, // Center Avatar in xs
+                }}
+              >
+                <img
+                  src={profilePic}
+                  alt="Profile Pic"
+                  style={{
+                    width: "100%",
+                    height: "102%", // Adjusted to 100% to fill the Avatar properly
+                    objectFit: "fill", // Maintain aspect ratio
+                  }}
                 />
-              </span>
-            </h1>
-          </Box>
+              </Avatar>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: 2,
+                  alignItems: "center",
+                  flexDirection: { xs: "column", sm: "row" },
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: {
+                      xs: 20, // Smaller font size for extra-small screens
+                      sm: 30, // Default font size for small screens and above
+                    },
+                  }}
+                >
+                  Welcome, I'm Basha Ksk
+                </Typography>
+
+                <Typography
+                  sx={{
+                    fontSize: {
+                      xs: 20, // Smaller font size for extra-small screens
+                      sm: 30, // Default font size for small screens and above
+                      color: "#d4a373",
+                      display: "inline-block",
+                    },
+                  }}
+                >
+                  <Typewriter
+                    words={[
+                      "I'm a Full Stack Developer",
+                      "Proficient in React & Node.js",
+                      "Passionate about coding",
+                      "Lifelong learner",
+                    ]}
+                    loop={true}
+                    cursor
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                  />
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
         </Paper>
       </Tilt>
     </Box>
